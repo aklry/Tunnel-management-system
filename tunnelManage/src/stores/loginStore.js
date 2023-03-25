4b9a6528450e63860aa1d180f745fd509a3cecca
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-25 21:38:23 +0800

    refactor: 创建useLoginStore对象存储数据

---
+++ tunnelManage/src/stores/loginStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+export const useLoginStore = defineStore('login', {
+    state: () => {
+        return {
+            token: '',
+            permission: '',
+            username: ''
+        }
+    }
+})
