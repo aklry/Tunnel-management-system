7ed908cfe5a1dcdedeffbc3bf0e28648087d09a3
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-25 21:49:51 +0800

    refactor: 引入仓库持久化插件

--- tunnelManage/src/main.js
+++ tunnelManage/src/main.js
@@ -5,10 +5,14 @@ import App from './App.vue'
 import router from './router'
 //引入公共样式
 import './assets/init.css'
+//引入仓库持久化插件
+import piniaPersist from 'pinia-plugin-persist'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPersist)
+
 app.use(router)
-
+app.use(pinia)
 app.mount('#app')
