7c9d7dcfb18a8504602ec083d0cffc2783dcf19b
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-25 21:50:19 +0800

    feat: 实现登录信息持久化功能

--- tunnelManage/src/stores/loginStore.js
+++ tunnelManage/src/stores/loginStore.js
@@ -7,5 +7,15 @@ export const useLoginStore = defineStore('login', {
             permission: '',
             username: ''
         }
+    },
+    //本地持久化
+    persist: {
+        enabled: true,
+        strategies: [
+            {
+                key: 'login',//自定义的key值，存储到本地时候的key
+                storage: localStorage,//选择存储方式
+            }
+        ]
     }
 })
