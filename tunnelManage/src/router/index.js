e751e108e2e33eef7a35f6939999d6f293791d06
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-25 22:11:46 +0800

    feat: 实现路由权限功能

--- tunnelManage/src/router/index.js
+++ tunnelManage/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView/index.vue'
 import Layout from '../views/Layout.vue'
 import LoginInfo from '../views/LoginInfo/index.vue'
+import { useLoginStore } from '../stores/loginStore'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,41 +10,65 @@ const router = createRouter({
       path: '/',
       name: 'Layout',
       component: Layout,
+      meta: {
+        requireAuth: true
+      },
       children: [
         {
           path: '/',
           name: 'home',
-          component: HomeView
+          component: HomeView,
+          meta: {
+            requireAuth: true
+          }
         },
         {
           path: '/project',
           name: 'Project',
-          component: () => import('../views/ProjectInfo/index.vue')
+          component: () => import('../views/ProjectInfo/index.vue'),
+          meta: {
+            requireAuth: true
+          }
         },
         {
           path: '/tunnel',
           name: 'Tunnel',
-          component: () => import('../views/TunnelInfo/index.vue')
+          component: () => import('../views/TunnelInfo/index.vue'),
+          meta: {
+            requireAuth: true
+          }
         },
         {
           path: '/work',
           name: 'Work',
-          component: () => import('../views/WorkManage/index.vue')
+          component: () => import('../views/WorkManage/index.vue'),
+          meta: {
+            requireAuth: true
+          }
         },
         {
           path: '/build',
           name: 'Build',
-          component: () => import('../views/BuildManage/index.vue')
+          component: () => import('../views/BuildManage/index.vue'),
+          meta: {
+            requireAuth: true
+          }
         },
         {
           path: '/geological',
           name: 'Geological',
-          component: () => import('../views/GeologicalInfo/index.vue')
+          component: () => import('../views/GeologicalInfo/index.vue'),
+          meta: {
+            requireAuth: true
+          }
         },
         {
           path: '/system',
           name: 'System',
-          component: () => import('../views/SystemManage/index.vue')
+          component: () => import('../views/SystemManage/index.vue'),
+          meta: {
+            requireAuth: true
+          }
         }
       ]
     },
@@ -54,5 +79,22 @@ const router = createRouter({
     }
   ]
 })
-
+/**
+ * 路由权限
+ */
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    const loginStore = useLoginStore()
+    //需要验证登录
+    if (loginStore.token) {
+      next()
+    } else {
+      next({
+        path: '/login'
+      })
+    }
+  } else {
+    next()
+  }
+})
 export default router
