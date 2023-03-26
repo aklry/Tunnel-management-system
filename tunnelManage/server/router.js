acc908b0c2ab2c51ad4acb63fc8c50d77c8155bd
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-26 19:07:06 +0800

    refactor: 编写接口获取数据库中的项目信息

--- tunnelManage/server/router.js
+++ tunnelManage/server/router.js
@@ -74,4 +74,26 @@ router.get('/line', (req, res) => {
         result: lineData
     })
 })
+
+/**
+ * 隧道信息查询
+ */
+router.get('/project/all', (req, res) => {
+    //分页
+    let page = url.parse(req.url, true).query.page || 1
+    const sql = `select * from project order by id desc limit 15 offset ${(page - 1) * 5}`
+    SQLConnect(sql, null, result => {
+        if (result.length > 0) {
+            res.send({
+                status: 200,
+                result
+            })
+        } else {
+            res.send({
+                status: 500,
+                msg: '暂无数据'
+            })
+        }
+    })
+})
 module.exports = router
