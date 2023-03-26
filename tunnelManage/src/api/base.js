84f430d5c0d12625c171a21318d853bf8e10d180
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-26 14:19:20 +0800

    refactor: 编写接口获取数据

--- tunnelManage/src/api/base.js
+++ tunnelManage/src/api/base.js
@@ -3,8 +3,9 @@
  */
 
 const base = {
-    baseUrl: 'http://localhost:3000',
-    login: '/api/login'
+    baseUrl: 'http://localhost:3000', //公共地址
+    login: '/api/login', //登录地址
+    router: '/api/router' //用户权限
 }
 
 export default base
--- tunnelManage/src/api/index.js
+++ tunnelManage/src/api/index.js
@@ -2,8 +2,22 @@ import axios from '../utils/request'
 import base from './base'
 
 const api = {
+    /**
+     * 登录
+     * @param {*} params 
+     * @returns 
+     */
     getLogin(params) {
         return axios.post(base.baseUrl + base.login,params)
+    },
+    /**
+     * 获取动态导航数据
+     * @param {用户权限} params 
+     */
+    getRouter(params) {
+        return axios.get(base.baseUrl + base.router, {
+            params
+        })
     }
 }
 
