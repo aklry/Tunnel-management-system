c36df4aa1cce61318a43394e144545cb35b1133a
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-26 14:18:01 +0800

    refactor: 根据用户权限编写左侧导航栏选项

---
+++ tunnelManage/server/data/admin.js
@@ -0,0 +1,39 @@
+module.exports = {
+    menus: [
+        {
+            path: "/",
+            name: "首页",
+            icon: "Document"
+        },
+        {
+            path: "/project",
+            name: "项目基础信息",
+            icon: "Discount"
+        },
+        {
+            path: "/tunnel",
+            name: "隧道设计信息",
+            icon: "Picture"
+        },
+        {
+            path: "/work",
+            name: "工作监督管理",
+            icon: "IceCreamRound"
+        },
+        {
+            path: "/build",
+            name: "施工监控检测",
+            icon: "Filter",
+        },
+        {
+            path: "/geological",
+            name: "超前地质预报",
+            icon: "ShoppingTrolley"
+        },
+        {
+            path: "/system",
+            name: "系统信息管理",
+            icon: "Wallet"
+        }
+    ]
+}
